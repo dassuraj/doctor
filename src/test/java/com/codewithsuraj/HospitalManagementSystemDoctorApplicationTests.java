a9fa0968cecab390a59199ed250ac2226b2301b5
dassuraj <dassuraj.np@gmail.com>
2024-05-13 11:10:41 +0545

    doctor initial commit without class

---
+++ src/test/java/com/codewithsuraj/HospitalManagementSystemDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithsuraj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
